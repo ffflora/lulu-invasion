76c5690d06a1ba2ff2c3711f1e7648c66e6e86f6
flora <avril_99@vii.im>
2020-06-19 00:34:23 -0700

    Add limit to the moves

--- ship.py
+++ ship.py
@@ -27,9 +27,9 @@ class Ship():
 
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.lu_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left  and self.rect.left > 0:
             self.center -= self.lu_settings.ship_speed_factor
         # update the centerx
         self.rect.centerx = self.center
